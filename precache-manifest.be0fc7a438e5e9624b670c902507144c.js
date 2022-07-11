610fa215de386534524418910e9a5a0110b30fd2
CyberFan-EK <merginskiy@gmail.com>
2022-07-11 21:33:48 +0300

    first commit

---
+++ precache-manifest.be0fc7a438e5e9624b670c902507144c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c6ca963c220c1f02c62f62baeb87d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b598ae046f01219d6cc0",
+    "url": "/static/css/main.b854109b.chunk.css"
+  },
+  {
+    "revision": "9dcfc9026e07b13431c3",
+    "url": "/static/js/2.eb6ee959.chunk.js"
+  },
+  {
+    "revision": "6828066851c6d85ff5a29bceaa486e0a",
+    "url": "/static/js/2.eb6ee959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b598ae046f01219d6cc0",
+    "url": "/static/js/main.6ef89b85.chunk.js"
+  },
+  {
+    "revision": "864964386b8fbd0a16b5",
+    "url": "/static/js/runtime-main.0fe5df00.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/static/media/preloader.c114fda3.gif"
+  }
+]);
